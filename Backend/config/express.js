2dbf2f7dae631216075c9633480989065db22e06
simonaester <simonaester@hotmail.it>
2020-08-06 12:27:55 +0200

    path

--- Backend/config/express.js
+++ Backend/config/express.js
@@ -12,3 +12,6 @@ module.exports = (app) => {
   app.use(cors());
   app.use(express.static(path.join(__dirname, 'build')));
 };
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
