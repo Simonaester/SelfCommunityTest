8efce05631568c30096a8fc5d1ef0df1d1f28ac5
simonaester <simonaester@hotmail.it>
2020-08-06 16:40:14 +0200

    progetto pulito

--- Backend/config/express.js
+++ Backend/config/express.js
@@ -10,8 +10,6 @@ module.exports = (app) => {
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
   app.use(cors());
-  app.use(express.static(path.join(__dirname, 'build')));
+
 };
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
